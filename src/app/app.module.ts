50f792d1dde3790b2fe85f32acc02f6a90eb1836
Marta <mavidelaru@gmail.com>
2020-02-11 15:36:15 +0100

    new repository

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,38 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule} from '@angular/common/http';
+import { ListadoPaisesComponent } from './components/listado-paises/listado-paises.component'
+import { PaisService } from './services/pais.service';
+import { FormsModule} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MultiSelectModule} from 'primeng/multiselect';
+
+
+//Modulos de PrimeNg
+import {TableModule} from 'primeng/table';
+import { ViajeComponent } from './components/viaje/viaje.component';
+import {ButtonModule} from 'primeng/button';
+import {DropdownModule} from 'primeng/dropdown';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListadoPaisesComponent,
+    ViajeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    TableModule,
+    MultiSelectModule,
+    DropdownModule,
+    ButtonModule
   ],
-  providers: [],
+  providers: [PaisService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
